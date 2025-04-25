ca0f0b759da4834910a3691735935a39cc2886f0
azizars71 <azizars71@gmail.com>
2025-04-25 14:16:01 +0700

    shell updated for push dockerhub

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Peringatan
+echo "PASTIKAN SUDAH EXPORT PASSWORD"
+
+# exit saat error
+set -e
+
+# variabel IMAGENAME
+IMAGE_NAME="azizars/karsajobs:latest"
+
+# build image
+echo "Membangun image Docker: $IMAGE_NAME"
+docker build -t $IMAGE_NAME .
+
+# login ke docker hub, password telah disimpan ke env
+echo "Login ke Docker Hub"
+echo "$PASSWORD_DOCKER_HUB" | docker login -u azizars --password-stdin
+
+# push ke Docker Hub
+echo "Push ke Docker Hub dengan nama $IMAGE_NAME"
+docker push $IMAGE_NAME
+
+# SELESAI
+echo "SELESAI"
