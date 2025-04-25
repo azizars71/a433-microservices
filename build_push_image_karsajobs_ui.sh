575cf8cf6dde8999f3209194a97cc1bcb97724ee
azizars71 <azizars71@gmail.com>
2025-04-25 14:44:12 +0700

    shell push

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# exit saat error
+set -e
+
+# loading env
+if [ -f .env ]; then
+    export $(cat .env | xargs)
+fi
+
+# variabel IMAGENAME
+IMAGE_NAME="ghcr.io/azizars71/karsajobs-ui:latest"
+
+# build image
+echo "Membangun image Docker: $IMAGE_NAME"
+docker build -t $IMAGE_NAME .
+
+# login ke Github, token telah disimpan ke env
+echo "Login ke Github"
+echo "$GHCR_TOKEN" | docker login ghcr.io -u azizars71 --password-stdin
+
+# push ke Github Packages
+echo "Push ke Github Packages dengan nama $IMAGE_NAME"
+docker push $IMAGE_NAME
+
+# SELESAI
+echo "SELESAI"
