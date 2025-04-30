bc825c76cde6235fb07a7f24f174db09e82911a7
azizars71 <azizars71@gmail.com>
2025-04-30 12:22:14 +0700

    dockerfile + script build-push

---
+++ build-push-order-service.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# exit saat error
+set -e
+
+# variabel IMAGENAME
+IMAGE_NAME="ghcr.io/azizars71/order-service:latest"
+
+# build image
+echo "Membangun image Docker: $IMAGE_NAME"
+docker build -t $IMAGE_NAME .
+
+# login ke Github, token telah disimpan ke env
+echo "Login ke Github"
+echo "$GHCR_TOKEN" | docker login ghcr.io -u azizars71 --password-stdin
+
+# push ke Github Packages
+echo "Push ke Github Packages dengan nama $IMAGE_NAME"
+docker push $IMAGE_NAME
+
+# SELESAI
+echo "SELESAI"
